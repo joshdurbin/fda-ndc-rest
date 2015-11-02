f90169a6da52d8beb50906ee1a201e0b2acdbcf9
Josh Durbin <durbinjo593@gmail.com>
2015-11-02 14:05:07 -0800

    Updated codec to allow for writes

--- src/main/groovy/io/durbs/ndc/codec/ProductCodec.groovy
+++ src/main/groovy/io/durbs/ndc/codec/ProductCodec.groovy
@@ -5,9 +5,12 @@ import io.durbs.ndc.domain.product.PharmacologicalClassCategory
 import io.durbs.ndc.domain.product.Product
 import io.durbs.ndc.domain.product.Substance
 import org.bson.BsonReader
+import org.bson.BsonString
+import org.bson.BsonValue
 import org.bson.BsonWriter
 import org.bson.Document
 import org.bson.codecs.Codec
+import org.bson.codecs.CollectibleCodec
 import org.bson.codecs.DecoderContext
 import org.bson.codecs.DocumentCodec
 import org.bson.codecs.EncoderContext
@@ -17,12 +20,12 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 
-class ProductCodec implements Codec<Product> {
+class ProductCodec implements CollectibleCodec<Product> {
 
   static final Codec<Document> documentCodec = new DocumentCodec()
 
   @Override
-  Product decode(BsonReader reader, DecoderContext decoderContext) {
+  Product decode(final BsonReader reader, final DecoderContext decoderContext) {
 
     final Document document = documentCodec.decode(reader, decoderContext)
 
@@ -57,14 +60,125 @@ class ProductCodec implements Codec<Product> {
   }
 
   @Override
-  void encode(BsonWriter writer, Product value, EncoderContext encoderContext) {
+  void encode(final BsonWriter writer, final Product product, final EncoderContext encoderContext) {
 
-    // DO NOTHING, DATA IS IMMUTABLE, READ ONLY
+    final Document document = new Document()
+
+    if (product.productID) {
+      document.put('productID', product.productID)
+    }
+
+    if (product.productNDC) {
+      document.put('productNDC', product.productNDC)
+    }
+
+    if (product.productTypeName) {
+      document.put('productTypeName', product.productTypeName)
+    }
+
+    if (product.proprietaryName) {
+      document.put('proprietaryName', product.proprietaryName)
+    }
+
+    if (product.proprietaryNameSuffix) {
+      document.put('proprietaryNameSuffix', product.proprietaryNameSuffix)
+    }
+
+    if (product.nonProprietaryName) {
+      document.put('nonProprietaryName', product.nonProprietaryName)
+    }
+
+    if (product.dosageFormName) {
+      document.put('dosageFormName', product.dosageFormName)
+    }
+
+    if (product.routeName) {
+      document.put('routeName', product.routeName)
+    }
+
+    if (product.startMarketingDate) {
+      document.put('startMarketingDate', Date.from(product.startMarketingDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()))
+    }
+
+    if (product.endMarketingDate) {
+      document.put('endMarketingDate', Date.from(product.endMarketingDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant()))
+    }
+
+    if (product.marketingCategoryName) {
+      document.put('marketingCategoryName', product.marketingCategoryName)
+    }
+
+    if (product.applicationNumber) {
+      document.put('applicationNumber', product.applicationNumber)
+    }
+
+    if (product.labelerName) {
+      document.put('labelerName', product.labelerName)
+    }
+
+    if (product.substances) {
+      document.put('substances', product.substances.collect { Substance substance ->
+
+        new Document()
+          .append('name', substance.name)
+          .append('activeNumeratorStrength', substance.activeNumeratorStrength)
+          .append('activeIngredUnit', substance.activeIngredUnit)
+      })
+    }
+
+    if (product.pharmacologicalClassCategories) {
+      document.put('pharmacologicalClassCategories', product.pharmacologicalClassCategories.collect { PharmacologicalClassCategory pharmacologicalClassCategory ->
+
+        new Document()
+          .append('name', pharmacologicalClassCategory.name)
+          .append('code', pharmacologicalClassCategory.code)
+      })
+    }
+
+    if (product.deaScheduleNumber) {
+      document.put('deaScheduleNumber', product.deaScheduleNumber)
+    }
+
+    if (product.packaging) {
+      document.put('packaging', product.packaging.collect { Packaging packaging ->
+
+        new Document()
+          .append('ndcPackageCode', packaging.ndcPackageCode)
+          .append('packageDescription', packaging.packageDescription)
+      })
+    }
+
+    documentCodec.encode(writer, document, encoderContext);
   }
 
   @Override
   Class<Product> getEncoderClass() {
     Product
+  }
+
+  @Override
+  Product generateIdIfAbsentFromDocument(final Product product) {
+
+    if (documentHasId(product)) {
+      product.setId(UUID.randomUUID() as String)
+    }
+
+    product
+  }
+
+  @Override
+  boolean documentHasId(final Product product) {
+    product.id
+  }
+
+  @Override
+  BsonValue getDocumentId(final Product product) {
+
+    if (!documentHasId(product)) {
+      throw new IllegalStateException('The product does not contain an _id');
+    }
+
+    new BsonString(product.id);
   }
 
   static final LocalDate BSON_DOCUMENT_TO_LOCALDATE(final Document document, final String propertyKey) {
